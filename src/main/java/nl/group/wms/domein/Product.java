2ea71a35b09c79a25fc6a2e9cee56d405849ece5
Kaihup <kai_91@live.nl>
2020-05-12 15:35:37 +0200

    test

--- src/main/java/nl/group/wms/domein/Product.java
+++ src/main/java/nl/group/wms/domein/Product.java
@@ -12,14 +12,15 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
     private String name;
-    private int width;              // mm
-    private int height;             // mm
-    private int length;             // mm
-    private int weight;             // gr
-    private int inStock;
     private int price;              // ct
-    private long eanCode;
-    private String description;
+//    private long eanCode;
+//    private String description;
+//    private int length;             // mm
+//    private int width;              // mm
+//    private int height;             // mm
+//    private int weight;             // gr
+//    private int inStock;            // TODO dit bij aanmaken van een nieuw productItem +1
+
 
     public long getId() {
         return id;
@@ -37,67 +38,5 @@ public class Product {
         this.name = name;
     }
 
-    public int getWidth() {
-        return width;
-    }
 
-    public void setWidth(int width) {
-        this.width = width;
-    }
-
-    public int getHeight() {
-        return height;
-    }
-
-    public void setHeight(int height) {
-        this.height = height;
-    }
-
-    public int getLength() {
-        return length;
-    }
-
-    public void setLength(int length) {
-        this.length = length;
-    }
-
-    public int getWeight() {
-        return weight;
-    }
-
-    public void setWeight(int weight) {
-        this.weight = weight;
-    }
-
-    public int getInStock() {
-        return inStock;
-    }
-
-    public void setInStock(int inStock) {
-        this.inStock = inStock;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-
-    public void setPrice(int price) {
-        this.price = price;
-    }
-
-    public long getEanCode() {
-        return eanCode;
-    }
-
-    public void setEanCode(long eanCode) {
-        this.eanCode = eanCode;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
