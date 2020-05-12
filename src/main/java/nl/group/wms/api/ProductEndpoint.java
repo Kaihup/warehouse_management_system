cf4fb57dbb2f12feda57e6bcf807bc2f17854db5
RickyWiermans <rickywiermans@gmail.com>
2020-05-12 15:24:11 +0200

    Tweede commit

--- src/main/java/nl/group/wms/api/ProductEndpoint.java
+++ src/main/java/nl/group/wms/api/ProductEndpoint.java
@@ -1,9 +1,12 @@
 package nl.group.wms.api;
 
+import nl.group.wms.Utils;
 import nl.group.wms.controller.ProductService;
 import nl.group.wms.domein.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,5 +19,11 @@ public class ProductEndpoint {
     public Iterable<Product> getAllProducts() {
         Iterable<Product> producten = ps.getAllProducts();
         return producten;
+    }
+
+    @PostMapping("/newProduct")
+    public void addNewProduct(@RequestBody Product product){
+        System.out.println(Utils.ic(Utils.ANSI_CYAN,"A new product: " + product.getName()));
+        ps.newProduct(product);
     }
 }
